84d785f0691d8a0cd3c0ae0f1d9c1e61c5029dab
Valentín <sanvalensan14@gmail.com>
2024-03-05 12:20:05 +0000

    primer commit

---
+++ Interfaz2/src/valentin_sanchez_lopez/Valentin_Sanchez_Lopez.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package valentin_sanchez_lopez;
+
+/**
+ *
+ * @author Valentín Sánchez López
+ */
+public class Valentin_Sanchez_Lopez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Reservas ventana = new Reservas();
+        ventana.setVisible(true);
+    }
+    
+}
