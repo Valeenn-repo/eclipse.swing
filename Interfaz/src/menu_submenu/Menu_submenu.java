2eb1dc4cc7b7b978274f506f1d7a240ed325606e
Valentín <sanvalensan14@gmail.com>
2024-03-05 12:18:09 +0000

    primer commit

---
+++ Interfaz/src/menu_submenu/Menu_submenu.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package menu_submenu;
+
+/**
+ *
+ * @author Valentín Sánchez López
+ */
+public class Menu_submenu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        menu ventana = new menu();
+        ventana.setVisible(true);
+    }
+    
+}
