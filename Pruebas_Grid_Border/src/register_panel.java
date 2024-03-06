c876cfd4ff4b903c9f353112a36f2a603fb10449
Valentín <sanvalensan14@gmail.com>
2024-03-06 12:54:26 +0000

    primer commit

---
+++ Pruebas_Grid_Border/src/register_panel.java
@@ -0,0 +1,106 @@
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import java.awt.SystemColor;
+import java.awt.Font;
+
+public class register_panel extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create the panel.
+	 */
+	
+	private JLabel nameLabel;
+	private JTextField nameField;
+	
+    private JLabel emailLabel; 
+    private JTextField emailField;
+
+    private JLabel passwordLabel;
+    private JPasswordField passwordField;
+
+    private JLabel confirmLabel;
+    private JPasswordField confirmField;
+
+  private JButton registerButton;
+  private JLabel lblNewLabel;
+  private JLabel lblNewLabel_1;
+  private JLabel lblNewLabel_2;
+	public register_panel() {
+		setBackground(SystemColor.inactiveCaptionBorder);
+		GridLayout gridLayout = new GridLayout(0,2);
+		gridLayout.setVgap(20);
+		setLayout(gridLayout);
+
+	    // Name
+	    nameLabel = new JLabel("Nombre:");
+	    nameLabel.setFont(new Font("Trebuchet MS", Font.BOLD | Font.ITALIC, 13));
+	    nameField = new JTextField(20);
+	    add(nameLabel);
+	    add(nameField);
+
+	    // Email
+	    emailLabel = new JLabel("Email:");
+	    emailLabel.setFont(new Font("Trebuchet MS", Font.BOLD | Font.ITALIC, 13));
+	    emailField = new JTextField(20); 
+	    add(emailLabel);
+	    add(emailField);
+
+	    // Password
+	    passwordLabel = new JLabel("Contraseña:");
+	    passwordLabel.setFont(new Font("Trebuchet MS", Font.BOLD | Font.ITALIC, 13));
+	    passwordField = new JPasswordField(20);
+	    add(passwordLabel); 
+	    add(passwordField);
+
+	    // Confirm password
+	    confirmLabel = new JLabel("Confirmar Contraseña:");
+	    confirmLabel.setFont(new Font("Trebuchet MS", Font.BOLD | Font.ITALIC, 13));
+	    confirmField = new JPasswordField(20);
+	    add(confirmLabel);
+	    add(confirmField);
+
+	    // Register button
+	    registerButton = new JButton("Registrarte");
+	    registerButton.addActionListener(new RegisterListener());
+	    
+	    lblNewLabel_1 = new JLabel(" ");
+	    add(lblNewLabel_1);
+	    
+	    lblNewLabel = new JLabel("  ");
+	    add(lblNewLabel);
+	    
+	    lblNewLabel_2 = new JLabel(" ");
+	    add(lblNewLabel_2);
+	    add(registerButton);
+
+
+	}
+	private class RegisterListener implements ActionListener {
+
+	    @Override
+	    public void actionPerformed(ActionEvent e) {
+	    	int result = JOptionPane.showConfirmDialog(
+	    			null, 
+	    			"Registro correcto. ¿Confirmar?", 
+	    			"",
+	    			JOptionPane.YES_NO_OPTION
+			);
+
+			if(result == JOptionPane.YES_OPTION) {
+			  // continue
+			}
+	    }
+
+	  }
+
+}
---
+++ Pruebas_Grid_Border/src/ventanaPrin.java
@@ -0,0 +1,173 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import java.awt.GridLayout;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JTextField;
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.SwingConstants;
+import java.awt.SystemColor;
+import javax.swing.JScrollPane;
+import java.awt.Font;
+import java.awt.Toolkit;
+
+public class ventanaPrin extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	register_panel panel = new register_panel();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ventanaPrin frame = new ventanaPrin();
+					frame.setVisible(true);
+					frame.setSize(1150, 750);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ventanaPrin() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage("D:\\Descargas\\irradiacion-de-alimentos (1).png"));
+		setTitle("SmartHouse");
+		inicializarComps();
+		
+	}
+
+	private void inicializarComps() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 790, 603);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panelPrincipal = new JPanel();
+		contentPane.add(panelPrincipal, BorderLayout.CENTER);
+		panelPrincipal.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panelSuperior = new JPanel();
+		panelSuperior.setBackground(new Color(255, 255, 255));
+		panelPrincipal.add(panelSuperior, BorderLayout.NORTH);
+		panelSuperior.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setBackground(SystemColor.menu);
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setIcon(new ImageIcon("D:\\Descargas\\2b072e99a37a44609089640290e314db.png"));
+		panelSuperior.add(lblNewLabel);
+		
+		JPanel panelLogin = new JPanel();
+		panelLogin.setOpaque(false);
+		panelSuperior.add(panelLogin, BorderLayout.EAST);
+		panelLogin.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblNewLabel_1 = new JLabel("  ");
+		panelLogin.add(lblNewLabel_1, BorderLayout.NORTH);
+		
+		JLabel lblNewLabel_2 = new JLabel(" ");
+		panelLogin.add(lblNewLabel_2, BorderLayout.SOUTH);
+		
+		JLabel lblNewLabel_3 = new JLabel("      ");
+		panelLogin.add(lblNewLabel_3, BorderLayout.EAST);
+		
+		JPanel panelGridLogin = new JPanel();
+		panelGridLogin.setOpaque(false);
+		panelLogin.add(panelGridLogin, BorderLayout.CENTER);
+		panelGridLogin.setLayout(new GridLayout(2, 4, 15, 0));
+		
+		JLabel lblNewLabel_4 = new JLabel("Tipo:");
+		panelGridLogin.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("Nombre:");
+		panelGridLogin.add(lblNewLabel_5);
+		
+		JLabel lblNewLabel_6 = new JLabel("Contraseña:");
+		panelGridLogin.add(lblNewLabel_6);
+		
+		JLabel lblNewLabel_7 = new JLabel("  ");
+		panelGridLogin.add(lblNewLabel_7);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Seleccione", "Admin", "Visitante"}));
+		panelGridLogin.add(comboBox);
+		
+		textField = new JTextField();
+		panelGridLogin.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		panelGridLogin.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Iniciar Sesión");
+		panelGridLogin.add(btnNewButton);
+		
+		JPanel panelInferior = new JPanel();
+		panelInferior.setBackground(new Color(255, 255, 255));
+		panelPrincipal.add(panelInferior, BorderLayout.SOUTH);
+		
+		JLabel lblNewLabel_9 = new JLabel("Desarrollado por: Valentín Sánchez");
+		lblNewLabel_9.setFont(new Font("Tahoma", Font.ITALIC, 12));
+		panelInferior.add(lblNewLabel_9);
+		
+		JPanel panelCentro = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) panelCentro.getLayout();
+		flowLayout.setVgap(15);
+		flowLayout.setHgap(10);
+		panelCentro.setBackground(SystemColor.inactiveCaptionBorder);
+		panelPrincipal.add(panelCentro, BorderLayout.CENTER);
+		
+		JLabel lblNewLabel_8 = new JLabel("");
+		lblNewLabel_8.setHorizontalTextPosition(SwingConstants.CENTER);
+		lblNewLabel_8.setHorizontalAlignment(SwingConstants.LEFT);
+		lblNewLabel_8.setIcon(new ImageIcon("D:\\Descargas\\icon-3317467_1280.png"));
+		panelCentro.add(lblNewLabel_8);
+		
+		JPanel panelFormulario = new JPanel();
+		panelFormulario.setOpaque(false);
+		panelFormulario.setPreferredSize(new Dimension(500, 400));
+		panelCentro.add(panelFormulario);
+		panelFormulario.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBackground(SystemColor.inactiveCaptionBorder);
+		scrollPane.setOpaque(false);
+		scrollPane.setBorder(null);
+		scrollPane.setBounds(0, 90, 500, 300);
+		panel = new register_panel();
+		GridLayout gridLayout = (GridLayout) panel.getLayout();
+		gridLayout.setVgap(25);
+		scrollPane.setViewportView(panel);
+		panelFormulario.add(scrollPane);
+		
+		JLabel lblNewLabel_10 = new JLabel("Registro \u2705");
+		lblNewLabel_10.setFont(new Font("Arial Unicode MS", Font.BOLD | Font.ITALIC, 25));
+		lblNewLabel_10.setBounds(201, 0, 172, 68);
+		panelFormulario.add(lblNewLabel_10);
+		
+	}
+}
